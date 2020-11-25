45499e4c7ba2f71b3d8dd9c701ba17807b8c7ece
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2020-11-25 18:50:00 -0500

    Allowed for tests to be built

---
+++ src/test/java/module-info.java
@@ -0,0 +1,8 @@
+module Simple.Cup.Builder.test {
+    requires org.junit.jupiter.api;
+
+    requires lombok;
+    requires com.github.jezza.toml;
+
+    exports tests;
+}
---
+++ src/test/java/tests/TestStuff.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestStuff {
+
+    @Test
+    public void test1() {
+        assertEquals(12, 12);
+    }
+}
