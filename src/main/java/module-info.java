b66012b6c7633def70b85dee1dc425e0a2a3ce9f
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2020-11-25 02:20:36 -0500

    Updated deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module tech.harmless.simplecupbuilder {
     requires lombok;
+    requires com.github.jezza.toml;
 }
--- src/main/java/tech/harmless/simplecupbuilder/SimpleCupBuilder.java
+++ src/main/java/tech/harmless/simplecupbuilder/SimpleCupBuilder.java
@@ -3,10 +3,8 @@ package tech.harmless.simplecupbuilder;
 public class SimpleCupBuilder {
 
     public SimpleCupBuilder() {
-
     }
 
     public static void main(String[] args) {
-
     }
 }
