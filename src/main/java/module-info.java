b69b31ad53fa9e4019e50698012d56d1b74c5010
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2020-12-07 03:15:37 -0500

    Added static annotation processing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,15 @@
 module tech.harmless.simplecupbuilder {
+    // Third-Party Annotations
     requires lombok;
+    requires org.jetbrains.annotations;
+    requires com.github.spotbugs.annotations;
+
+    // Third-Party Libs
     requires com.github.jezza.toml;
     requires com.google.gson;
 
+    // Java Libs
     requires java.base;
+
+    // Exports
 }
